0b71183ac21f86d3a2bf931f38c7e6e6ce9e8af2
noormdafsar <nooruddin.mdafsar.it25@heritageit.edu.in>
2024-01-10 17:27:49 +0530

    1st commit

---
+++ models/doctor.models.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+const doctorSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    qualification: {
+        type: String,
+        required: true
+    },
+    salary: {
+        type: String,
+        required: true
+    },
+    experienceInYear: {
+        type: String,
+        required: true
+    },
+    workInHospital: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Hospital'
+    }, 
+}, {timestamps: true})
+export const Doctor = mongoose.model('Doctor', doctorSchema)
